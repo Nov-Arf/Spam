6b1810a97c33a00838b05dc3b620c667bc02b791
Nov-Arf <134609875+Nov-Arf@users.noreply.github.com>
2023-06-02 20:22:58 +0700

    Add files via upload

---
+++ module.sh
@@ -0,0 +1,12 @@
+pip install requests
+pip install bs4
+pip install datetime
+pip install rich
+pip install colorama
+pip install pyfiglet
+pip install tqdm
+pip install inquirer
+
+sleep 3
+
+python run.py
